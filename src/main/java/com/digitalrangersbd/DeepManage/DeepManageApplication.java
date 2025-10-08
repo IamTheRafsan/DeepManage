3c1e1641661b49a8186c9b7fbce762d6cc8592e5
Rafsan <sending.to.evan@gmail.com>
2025-10-08 14:43:26 +0600

    initial commit

---
+++ src/main/java/com/digitalrangersbd/DeepManage/DeepManageApplication.java
@@ -0,0 +1,15 @@
+package com.digitalrangersbd.DeepManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepManageApplication {
+
+	public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+	}
+
+}
---
+++ src/test/java/com/digitalrangersbd/DeepManage/DeepManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalrangersbd.DeepManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
